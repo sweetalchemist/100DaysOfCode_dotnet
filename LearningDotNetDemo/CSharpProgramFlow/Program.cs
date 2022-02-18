ce881827ebbd66e92d43305927a0a59ec6a1cbdb
sweetalchemist <simplyramanan@gmail.com>
2022-02-18 13:28:34 +0530

    Statements

---
+++ LearningDotNetDemo/CSharpProgramFlow/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+double responseRate;
+double unansweredRate;
+
+responseRate = Q1Results.NumberResponded / Q1Results.NumberSurveyed;
+
+unansweredRate = Q1Results.NumberSurveyed - Q1Results.NumberResponded;
+
+Console.WriteLine($" Response Rate is {responseRate}; Unanswered Rate is:  {unansweredRate}");
+
+Console.ReadKey();
