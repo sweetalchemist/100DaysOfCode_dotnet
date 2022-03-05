1e094e95f6840289eb69c966a46f41545b24baf6
sweetalchemist <simplyramanan@gmail.com>
2022-03-05 20:19:33 +0530

    async continuance

---
+++ LearningDotNetDemo/CSharpAsync/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Hello World!");
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"Main Thread: {DateTime.Now.ToString()}");
+    var result = await DoSomething();
+    Console.WriteLine($"Result :{result}");
+
+}
+
+async Task<string> DoSomething()
+{
+    Console.WriteLine($"Sub Thread: {DateTime.Now.ToString()}");
+    await Task.Run( () => Thread.Sleep(10000));
+    return DateTime.Now.ToString();
+}
+
+Console.ReadKey();
